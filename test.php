cd6ffe157d6d80f4df1e3bc4054a75dd58a9145a
VojtaSkarin <54547074+VojtaSkarin@users.noreply.github.com>
2019-09-15 14:46:44 +0200

    Update test.php

--- test.php
+++ test.php
@@ -2,14 +2,34 @@
 
 <head>
 <title>Pravoslavný kalendář</title>
+<link rel = "icon" href = "icon.jpg">
 </head>
 
 <body>
 <?php
 class Datum {
-	function Datum($den, $mesic) {
+	function Datum() {
+		$this->den = 0;
+		$this->mesic = 0;
+		$this->rok = 0;
+		$this->den_v_tydnu = 0;
+	}
+	
+	function nastavit($den, $mesic, $rok) {
 		$this->den = $den;
 		$this->mesic = $mesic;
+		$this->rok = $rok;
+	}
+	
+	function dnesni_datum() {
+		$pole = explode(" ", date("d m o N"));
+		$this->nastavit((int) $pole[0], (int) $pole[1], (int) $pole[2]);
+		$this->den_v_tydnu = (int) $pole[3];
+	}
+	
+	function vypsat() {
+		echo $this->den, ". ", $this->mesic, ". ", $this->rok,
+			" den: ", $this->den_v_tydnu, "<br>";
 	}
 }
 
@@ -34,22 +54,20 @@ class Den {
 	function Den() {
 		$this->s_den = 0;
 		$this->s_mesic = 0;
+		$this->s_rok = 0;
 		$this->n_den = 0;
 		$this->n_mesic = 0;
-		$this->den_v_tydnu = 0;
 		$this->n_rok = 0;
-		$this->s_rok = 0;
+		$this->den_v_tydnu = 0;
 		$this->hlas = 0;
+		$this->tyden = 0;
+		$this->dpp = 0;
 		$this->tk = array();
 	}
 	
-	function nastavit_datum($posun, $datum_paschy) {	
-		$amax = array(31, ($this->n_rok % 4 == 0) ? 29 : 28, 31,
-			30, 31, 30, 31, 31, 30, 31, 30, 31);	
-		
-		$pole = explode(" ", date("d m o N"));
-		$this->n_den = (int) $pole[0];
-		$this->n_mesic = $this->s_mesic = (int) $pole[1];
+	function dnesni_den($posun, $datum_paschy, $datum) {
+		$this->n_den = $datum->den;
+		$this->n_mesic = $this->s_mesic = $datum->mesic;
 		if ($this->n_den >= 14)
 		{
 			$this->s_den = $this->n_den - $posun;
@@ -57,14 +75,14 @@ class Den {
 		else
 		{
 			$this->s_mesic += ($this->s_mesic != 1) ? -1 : 11;
-			$this->s_den = amax[$this->s_mesic - 1] - posun + $this->n_den;
+			$this->s_den = dvm($this->s_mesic, $this->s_rok) - $posun + $this->n_den;
 		}
-		$this->n_rok = (int) $pole[2];
+		$this->n_rok = $datum->rok;
 		$this->s_rok = $this->n_rok + 5508 + (($this->n_mesic >= 8
 			&& $this->n_den >= 14) ? 1 : 0);
-		$this->den_v_tydnu = (int) $pole[3];
-		$this->vypocist_hlas($datum_paschy, $amax);
-		$this->nacist_tk();
+		$this->den_v_tydnu = $datum->den_v_tydnu;
+		$this->hlas_a_tyden($datum_paschy, $datum);
+		//$this->nacist_tk(); --TODO
 	}
 	
 	function nacist_tk() {
@@ -89,13 +107,45 @@ class Den {
 			"říjen", "listopad", "prosinec");
 		
 		echo $den_v_tydnu[$this->den_v_tydnu - 1], "<br>";
-		echo "Starý styl:", $this->s_den, ". ", $mesic[$this->s_mesic - 1], "<br>";
+		echo "Starý styl: ", $this->s_den, ". ", $mesic[$this->s_mesic - 1], "<br>";
 		echo "Nový styl: ", $this->n_den, ". ", $mesic[$this->n_mesic - 1], "<br>";
 		echo $this->s_rok, " let od stvoření světa", "<br>";
 		echo $this->n_rok, " let od narození Krista", "<br>";
-		echo "Hlas ", $this->hlas, ".<br>";
-		echo "<br><br>";
 		
+		// Týdnování
+		if ($this->tyden == 1 && $this->den_v_tydnu != 7)
+		{
+			echo "Světlý týden", "<br>";
+		}
+		if ($this->tyden > 1 && $this->tyden < 8)
+		{
+			echo $this->tyden, ". ", ($this->den_v_tydnu == 7) ? "neděle" : "týden", " po Pasše", "<br>";
+		}
+		if ($this->tyden >= 8)
+		{
+			$tyden = $this->tyden - (($this->den_v_tydnu == 7) ? 8 : 7);
+			if ($tyden)
+			{
+				echo $tyden, ". ", ($this->den_v_tydnu == 7) ? "neděle" : "týden", " po Padesátnici", "<br>";
+			}
+			else
+			{
+				echo "8. neděle po Pasše", "<br>";
+			}
+		}
+		// Konec týdnování
+		
+		if ($this->hlas)
+		{
+			echo "Hlas ", $this->hlas, ".<br>";
+		}
+		
+		echo "DPP: ", $this->dpp, "<br>";
+		echo "<br><br>";
+		$this->vypsat_cteni();
+		
+		// Jména svatých, tropary a kondaky - TODO
+		/*
 		for ($i = 0; $i < count($this->tk); $i++)
 		{
 			$svaty = $this->tk[$i];
@@ -103,34 +153,132 @@ class Den {
 			echo "Tropar: <br>", $svaty->tropar->text, "<br>", $svaty->tropar->hlas, ". hlas<br><br>";
 			echo "Kondak: <br>", $svaty->kondak->text, "<br>", $svaty->kondak->hlas, ". hlas<br><br>";
 		}
+		*/
 	}
 	
-	function vypocist_hlas($datum_paschy, $amax) {
-		if ($this->n_mesic > $datum_paschy->mesic || ($this->n_mesic ==
-			$datum_paschy->mesic && $this->n_den >= $datum_paschy->den))
-		{
-			$pocet_dni = 0;
-			for ($i = $datum_paschy->mesic + 1; $i < $this->n_mesic; $i++)
-			{
-				$pocet_dni += $amax[$i - 1];
-			}
-			$pocet_dni += $amax[$datum_paschy->mesic - 1] - $datum_paschy->den
-				+ $this->n_den;
-			
-			$this->hlas = intdiv($pocet_dni, 7) % 8 + (($this->den_v_tydnu == 0) ? 2 : 1);
-		}
-		else
-		{
-			
-		}
+	function hlas_a_tyden($datum_paschy, $dnesni_datum) {
+		$this->dpp = rozdil_dnu($datum_paschy, $dnesni_datum);
+		$this->tyden = podil($this->dpp) + (($this->den_v_tydnu == 7) ? 1 : 0);
+		$this->hlas = (($this->tyden - 2) % 8) + 1;
+	}
+	
+	function vypsat_cteni() {
+		$jmeno_souboru = sprintf("data/e/%d.dat", intdiv($this->dpp, 7));
+		$soubor = fopen($jmeno_souboru, "r");
+		$text = explode("***", fread($soubor, filesize($jmeno_souboru)))[$this->dpp %7];
+		echo $text, "<br>";
 	}
 }
-$posun = 13;
-$datum_paschy = new Datum(28, 4);
 
-$den = new Den();
-$den->nastavit_datum($posun, $datum_paschy);
-$den->vypsat();
+function vratit_datum($den=0, $mesic=0, $rok=0) {
+	$dnesni_datum = new Datum();
+	$dnesni_datum->dnesni_datum();
+	if (!$den)
+	{
+		return $dnesni_datum;
+	}
+	
+	$datum = new Datum();
+	$datum->nastavit($den, $mesic, $rok);
+	
+	$datum->den_v_tydnu = (($dnesni_datum->den_v_tydnu - rozdil_dnu($datum, $dnesni_datum) % 7) + 7) % 7;
+	if (!$datum->den_v_tydnu)
+	{
+		$datum->den_v_tydnu = 7;
+	}
+	
+	return $datum;
+}
+
+function rozdil_dnu($prvni, $druhy) {
+	$zmena = porovnej($prvni, $druhy);
+	
+	if ($zmena)
+	{
+		$zaloha = $prvni;
+		$prvni = $druhy;
+		$druhy = $zaloha;
+	}
+	
+	$pocet_dni = 0;
+	
+	if ($prvni->mesic != $druhy->mesic)
+	{
+		for ($i = $prvni->mesic + 1; $i < $druhy->mesic; $i++)
+		{
+			$pocet_dni += dvm($i, $prvni->rok);
+		}
+		
+		$pocet_dni += dvm($prvni->mesic, $prvni->rok) - $prvni->den;
+		$pocet_dni += $druhy->den;
+	}
+	else
+	{
+		$pocet_dni += $druhy->den - $prvni->den;
+	}
+	
+	if ($zmena)
+	{
+		$pocet_dni = -$pocet_dni;
+	}
+	
+	return $pocet_dni;
+}
+
+function dvm($mesic, $rok) {
+	return array(31, ($rok % 4 == 0) ? 29 : 28, 31,
+			30, 31, 30, 31, 31, 30, 31, 30, 31)[$mesic - 1];
+}
+
+function podil($cislo) {
+	$podil = intdiv($cislo, 7);
+	if ($cislo % 7 != 0)
+	{
+		$podil++;
+	}
+	return $podil;
+}
+
+function porovnej($prvni, $druhy) {
+	
+	if ($prvni->rok > $druhy->rok)
+	{
+		return 1;
+	}
+	else if ($prvni->rok < $druhy->rok)
+	{
+		return 0;
+	}
+	
+	if ($prvni->mesic > $druhy->mesic)
+	{
+		return 1;
+	}
+	else if ($prvni->mesic < $druhy->mesic)
+	{
+		return 0;
+	}
+	
+	if ($prvni->den > $druhy->den)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+function main() {		
+	$posun = 13;
+	$datum_paschy = vratit_datum(28, 4, 2019);
+
+	$den = new Den();
+	$den->dnesni_den($posun, $datum_paschy, vratit_datum(9, 9, 2019));
+	$den->vypsat();
+}
+
+main();
 ?>
 </body>
 
